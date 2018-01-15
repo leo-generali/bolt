a429028d70a4e54fa772e17935cdaa25aff66ea6
Leo Generali <leogen17@gmail.com>
2018-01-15 13:28:40 -0500

    Clean up pace string creator

--- src/helpers/math.js
+++ src/helpers/math.js
@@ -1,10 +1,6 @@
 export function calculatePaceString(minutes, seconds) {
   const minutesTrunc = Math.trunc(seconds / 60);
-  let secondsRemainder = Math.round((seconds % 60) * 100) / 100;
-
-  if( secondsRemainder < 10 ) {
-    secondsRemainder = `0${secondsRemainder}`
-  }
-
-  return `${minutesTrunc}:${secondsRemainder}`
+  const secondsTrunc = Math.round((seconds % 60) * 100) / 100;
+  const secondsRemainder = secondsTrunc > 10 ? secondsTrunc : `0${secondsTrunc}`;
+  return `${minutesTrunc}:${secondsRemainder}`;
 }
