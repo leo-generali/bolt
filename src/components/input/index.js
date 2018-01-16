4968a65c610956b1f08080d77dcd7ac74cf40227
Leo Generali <leogen17@gmail.com>
2018-01-15 23:05:41 -0500

    Add basic styling for easier testing

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -4,11 +4,13 @@ import style from './style.scss';
 export default class Input extends Component {
 	render() {
 		return (
-			<input 
+			<input
+				className={style.numInput}
 				type={this.props.type}
 				name={this.props.name}
 				step={this.props.step}
 				value={this.props.value}
+				min={this.props.type === 'number' ? '0' : ''}
 				onChange={(e) => this.props.handleChange(e)}
 			/>
 		);
